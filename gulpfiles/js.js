ff86ae2b85389427d548d6e7da17ae674e791490
Kimberly Grey <kimandthemachine@gmail.com>
2019-11-11 17:47:59 +0000

    Replace Rollup with Browserify

--- gulpfiles/js.js
+++ gulpfiles/js.js
@@ -1,40 +1,40 @@
 const config = require('../package.json');
 const gulp = require('gulp');
+const browserify = require('browserify');
 const sourcemaps = require('gulp-sourcemaps');
 const argv = require('yargs').argv;
 const gulpif = require('gulp-if');
+const vinylSource = require('vinyl-source-stream');
+const vinylBuffer = require('vinyl-buffer');
+const uglify = require('gulp-uglify');
 
 gulp.task('js:clean', () => {
-	const del = require('del');
-	return del(['./dist/js']);
+    const del = require('del');
+    return del(['./dist/js']);
 });
 
 gulp.task('js:watch', () => {
-	gulp.watch('./src/js', gulp.parallel('js:compile'));
+    gulp.watch('./src/js', gulp.parallel('js:compile'));
 });
 
-gulp.task('js:compile', (cb) => {
-	const rollup = require('gulp-better-rollup');
-	const babel = require('gulp-babel');
-	const uglify = require('gulp-uglify');
-	gulp.src('./src/js/**/*')
-		.pipe(sourcemaps.init())
-		.pipe(rollup({
-			name: config.name,
-			format: 'umd'
-		}))
-		.pipe(babel({
-            presets: ['@babel/env']
-        }))
-		.pipe(gulpif(argv.minify, uglify()))
-		.pipe(sourcemaps.write('.'))
-		.pipe(gulp.dest('./dist/js'));
-	cb();
+gulp.task('js:compile', () => {
+    const b = browserify({
+        entries: ['./src/js/all.js']
+    });
+    return b
+        .transform('babelify', { presets: ['@babel/preset-env'] })
+        .bundle()
+        .pipe(vinylSource('all.js'))
+        .pipe(vinylBuffer())
+        .pipe(sourcemaps.init({ loadMaps: true }))
+        .pipe(gulpif(argv.minify, uglify()))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('./dist/js'));
 });
 
 gulp.task('js',
-	gulp.series(
-		'js:clean',
-		'js:compile'
-	)
+    gulp.series(
+        'js:clean',
+        'js:compile'
+    )
 );
