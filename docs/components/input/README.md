6bec63dffd0d05c154e4c7270ca2ee80af135048
Kimberly Grey <kimandthemachine@gmail.com>
2019-11-12 10:05:18 +0000

    Move files around to way Metalsmith likes more

---
+++ docs/components/input/README.md
@@ -0,0 +1,9 @@
+## When to use this component
+
+## When to consider something else
+
+## Guidance
+
+## Accessibility
+
+## Browser considerations
---
+++ docs/components/input/input.js
@@ -0,0 +1,6 @@
+export default class Input {
+	constructor($module) {
+		this.$module = $module;
+		console.log('init\'d on input', this.$module);
+	}
+}
