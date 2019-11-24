01e48df2df19109f70becd4303b0921ca1291ffd
Kimberly Grey <kimandthemachine@gmail.com>
2019-11-24 12:09:38 +0000

    Update add-component script to add a JS and SASS boilerplates.

--- bin/add-component.sh
+++ bin/add-component.sh
@@ -33,26 +33,32 @@ touch "$COMPONENT_PATH/$COMPONENT_NAME_LOWER.njk"
 touch "$COMPONENT_PATH/$COMPONENT_NAME_LOWER.config.yaml"
 touch "$COMPONENT_PATH/README.md"
 touch "$COMPONENT_PATH/params.yaml"
+touch "$COMPONENT_PATH/$COMPONENT_NAME_LOWER.js"
+touch "$COMPONENT_PATH/_$COMPONENT_NAME_LOWER.scss"
 
 # Prepopulate macro
 cat <<EOF > "$COMPONENT_PATH/macro.njk"
 {% macro ${NAMESPACE}${COMPONENT_NAME_CAMEL}(params) %}
-	{%- include "./template.njk" -%}
+    {%- include "./template.njk" -%}
 {% endmacro %}
 EOF
 
 # Prepopulate template
 cat <<EOF > "$COMPONENT_PATH/template.njk"
 <div
-  data-module="${NAMESPACE}-${COMPONENT_NAME_LOWER}"
-  class="${NAMESPACE}-${COMPONENT_NAME_LOWER} {%- if params.classes %} {{ params.classes }}{% endif %}"
-  {%- for attribute, value in params.attributes %} {{ attribute }}="{{ value }}"{% endfor %}>
+    data-module="${NAMESPACE}-${COMPONENT_NAME_LOWER}"
+    class="${NAMESPACE}-${COMPONENT_NAME_LOWER} {%- if params.classes %} {{ params.classes }}{% endif %}"
+    {%- for attribute, value in params.attributes %} {{ attribute }}="{{ value }}"{% endfor %}>
 </div>
 EOF
 
-# Prepopulate template
+# Prepopulate demo file
 cat <<EOF > "$COMPONENT_PATH/$COMPONENT_NAME_LOWER.njk"
 {% from "${COMPONENT_NAME_LOWER}/macro.njk" import ${NAMESPACE}${COMPONENT_NAME_CAMEL} %}
+
+{{ ${NAMESPACE}${COMPONENT_NAME_CAMEL}({
+    
+}) }}
 EOF
 
 # Prepopulate readme
@@ -68,6 +74,7 @@ cat <<EOF > "$COMPONENT_PATH/README.md"
 ## Browser considerations
 EOF
 
+# Prepopulate parameter docs
 cat <<EOF > "$COMPONENT_PATH/params.yaml"
 params:
   - name: attributes
@@ -78,4 +85,21 @@ params:
     type: string
     required: false
     description: Classes to add to the component.
+EOF
+
+# Prepopulate Sass file
+cat <<EOF > "$COMPONENT_PATH/_$COMPONENT_NAME_LOWER.scss"
+.${NAMESPACE}-${COMPONENT_NAME_LOWER} {
+    // Put code here
+}
+EOF
+
+# Prepopulate JS file
+cat <<EOF > "$COMPONENT_PATH/$COMPONENT_NAME_LOWER.js"
+export default class ${COMPONENT_NAME_CAMEL} {
+    constructor(\$module) {
+        this.\$module = \$module;
+        // Put code here
+    }
+}
 EOF
