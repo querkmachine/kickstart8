39057a5ccf8bc456c66bfc3a0404069d5a0b2387
Kimberly Grey <kimandthemachine@gmail.com>
2020-05-31 14:31:10 +0100

    Modify default JS file and initialisation.

--- src/js/all.js
+++ src/js/all.js
@@ -4,34 +4,29 @@
  * class on that component. It will only do this if the component is explicitly
  * listed here, and only if it finds the component on the page, however it
  * loads the JS for *ALL* components, as they're all imported at once.
- * 
+ *
  * You can keep JS filesizes small by creating different entry files that only
  * include the components needed for a page or section.
- * 
+ *
  * This file also exports all component classes so that they can be initialized
  * individually, if necessary.
  *
  * `window.theNamespace.initAll()` (where `theNamespace` is the configured name
  * of the npm package.)
+ */
 
-import Datepicker from '../../components/datepicker/datepicker';
+// import Datepicker from '../../components/datepicker/datepicker';
 
 function initAll(options) {
-	options = typeof options !== 'undefined' ? options: {};
+  options = typeof options !== "undefined" ? options : {};
 
-	// Scope initialization to only certain parts of the page
-	// Defaults to entire document if not set
-	const scope = typeof options.scope !== 'undefined' ? options.scope : document;
+  // Scope initialization to only certain parts of the page
+  // Defaults to entire document if not set
+  const scope = typeof options.scope !== "undefined" ? options.scope : document;
 
-	scope.querySelectorAll('[data-module="kickstart-datepicker"]').forEach(datepicker => {
-		new Datepicker(datepicker);
-	});
-
+  // scope.querySelectorAll('[data-module="kickstart-datepicker"]').forEach(datepicker => {
+  // 	new Datepicker(datepicker);
+  // });
 }
 
-export {
-	initAll,
-	Datepicker
-}
-
-*/
+export { initAll };
